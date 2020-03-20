86197c2371ce3251aeb9e505b2f3c94cb8089aab
AvBorikDeveloper <avboris28@gmail.com>
2020-03-21 01:29:03 +1300

    Set up the connection to back4app parse server. Tested the connection, working fine

--- app/src/main/java/com/avborik28/instagramclone/signUp.java
+++ app/src/main/java/com/avborik28/instagramclone/signUp.java
@@ -3,8 +3,13 @@ package com.avborik28.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class signUp extends AppCompatActivity {
 
@@ -15,4 +20,19 @@ public class signUp extends AppCompatActivity {
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
+
+//    public void helloWorldTapped (View view){
+//        ParseObject boxer = new ParseObject("Boxer");
+//        boxer.put("punch_speed", 200);
+//        boxer.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e == null){
+//                    Toast.makeText(signUp.this, "Boxer object is saved successfully", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
+
+    // Second test project
 }
