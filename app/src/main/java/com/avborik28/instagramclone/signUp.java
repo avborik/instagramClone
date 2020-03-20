3ba8e849d55989f2b071bd0461fd864b883b7a85
AvBorikDeveloper <avboris28@gmail.com>
2020-03-21 01:42:18 +1300

    Second test object with additional values was sent to server The server received the object

--- app/src/main/java/com/avborik28/instagramclone/signUp.java
+++ app/src/main/java/com/avborik28/instagramclone/signUp.java
@@ -21,7 +21,7 @@ public class signUp extends AppCompatActivity {
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
-//    public void helloWorldTapped (View view){
+  public void helloWorldTapped (View view){
 //        ParseObject boxer = new ParseObject("Boxer");
 //        boxer.put("punch_speed", 200);
 //        boxer.saveInBackground(new SaveCallback() {
@@ -32,7 +32,22 @@ public class signUp extends AppCompatActivity {
 //                }
 //            }
 //        });
-//    }
+        // Second test project
+        final ParseObject kickBoxer = new ParseObject("KickBoxer");
+        kickBoxer.put("name","Bearka");
+        kickBoxer.put("punch_speed", 1000);
+        kickBoxer.put("punch_Power", 2000);
+        kickBoxer.put("kickSpeed", 3000);
+        kickBoxer.put("kickPower", 4000);
+        kickBoxer.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Toast.makeText(signUp.this, kickBoxer.get("name") + " KickBoxer is saved successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+  }
 
-    // Second test project
+
 }
