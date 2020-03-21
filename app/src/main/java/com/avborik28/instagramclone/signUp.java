21a649d05e9e01d13c2987303fa3a753a21249f3
AvBorikDeveloper <avboris28@gmail.com>
2020-03-22 05:27:03 +1300

    Finished the design of Login page, Did the sign up logic, tested, all working Started to do the Login logic

--- app/src/main/java/com/avborik28/instagramclone/signUp.java
+++ app/src/main/java/com/avborik28/instagramclone/signUp.java
@@ -2,27 +2,74 @@ package com.avborik28.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
-
-
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 public class signUp extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtEmail, edtUsername, edtPassword;
+    private Button btnSignUp, btnLogin;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        setTitle("Sign UP");
+
+        edtEmail = findViewById(R.id.edtEnterEmail);
+        edtUsername = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnSignUp:
+                final ParseUser appUser = new ParseUser();
+                appUser.setEmail(edtEmail.getText().toString());
+                appUser.setUsername(edtUsername.getText().toString());
+                appUser.setPassword(edtPassword.getText().toString());
 
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null){
+                            FancyToast.makeText(signUp.this,
+                                    appUser.getUsername() + " is signed up",
+                                    Toast.LENGTH_LONG,FancyToast.SUCCESS, true).show();
+                        } else {
+                            FancyToast.makeText(signUp.this,
+                                    "There was an error, Debil: " + e.getMessage(),
+                                    Toast.LENGTH_LONG,FancyToast.ERROR, true).show();
+
+                        }
+                    }
+                });
+                break;
+            case R.id.btnLogin:
+                Intent intent = new Intent(signUp.this, Login.class );
+                startActivity(intent);
+                break;
+        }
     }
 }
