43ec4e91c7023b114254a77c8aa0977d548a6184
AvBorikDeveloper <avboris28@gmail.com>
2020-03-22 02:09:57 +1300

    Added the logout button Added the welcome message and implemented the transition between activities

--- app/src/main/java/com/avborik28/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/avborik28/instagramclone/SignUpLoginActivity.java
@@ -1,5 +1,6 @@
 package com.avborik28.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,8 +48,11 @@ public class SignUpLoginActivity extends AppCompatActivity {
                     public void done(ParseException e) {
                         if(e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,appUser.get("username") + " is signed successfully",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
                         } else {
                             FancyToast.makeText(SignUpLoginActivity.this,"Error, debil",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
                         }
                     }
                 });
@@ -63,6 +67,8 @@ public class SignUpLoginActivity extends AppCompatActivity {
                     public void done(ParseUser user, ParseException e) {
                         if(user != null && e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,user.get("username") + " is Logged successfully",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
                         } else {
                             FancyToast.makeText(SignUpLoginActivity.this,"Error, debil",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
                         }
--- app/src/main/java/com/avborik28/instagramclone/WelcomeActivity.java
+++ app/src/main/java/com/avborik28/instagramclone/WelcomeActivity.java
@@ -3,12 +3,31 @@ package com.avborik28.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView txtWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        txtWelcome = findViewById(R.id.txtWelcome);
+
+        txtWelcome.setText("Welcome !!!" + ParseUser.getCurrentUser().get("username"));
+
+        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                finish();
+
+            }
+        });
     }
 }
