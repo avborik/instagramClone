510c0d288ebfea3791d29cae02c87447e89aea23
AvBorikDeveloper <avboris28@gmail.com>
2020-03-23 04:07:31 +1300

    Added input validation methods for sign up page, Added hide keyboard methods for sign up page.

--- app/src/main/java/com/avborik28/instagramclone/signUp.java
+++ app/src/main/java/com/avborik28/instagramclone/signUp.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +36,19 @@ public class signUp extends AppCompatActivity implements View.OnClickListener {
         edtUsername = findViewById(R.id.edtUserName);
         edtPassword = findViewById(R.id.edtPassword);
 
+        edtPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_ENTER
+                        && event.getAction() == KeyEvent.ACTION_DOWN){
+                    onClick(btnSignUp);
+                }
+
+                return false;
+            }
+        });
+
         btnSignUp = findViewById(R.id.btnSignUp);
         btnLogin = findViewById(R.id.btnLogin);
 
@@ -46,36 +61,56 @@ public class signUp extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnSignUp:
-                final ParseUser appUser = new ParseUser();
-                appUser.setEmail(edtEmail.getText().toString());
-                appUser.setUsername(edtUsername.getText().toString());
-                appUser.setPassword(edtPassword.getText().toString());
+                if(edtEmail.getText().toString().equals("") ||
+                        edtUsername.getText().toString().equals("")
+                        || edtPassword.getText().toString().equals("")) {
+                    FancyToast.makeText(signUp.this,
+                            "The username, password and email are required, Debil",
+                            Toast.LENGTH_LONG,FancyToast.INFO, true).show();
+                } else {
+                    final ParseUser appUser = new ParseUser();
+                    appUser.setEmail(edtEmail.getText().toString());
+                    appUser.setUsername(edtUsername.getText().toString());
+                    appUser.setPassword(edtPassword.getText().toString());
 
-                final ProgressDialog progressDialog = new ProgressDialog(this);
-                progressDialog.setMessage("Signing up" + edtUsername.getText().toString());
-                progressDialog.show();
+                    final ProgressDialog progressDialog = new ProgressDialog(this);
+                    progressDialog.setMessage("Signing up" + edtUsername.getText().toString());
+                    progressDialog.show();
 
-                appUser.signUpInBackground(new SignUpCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null){
-                            FancyToast.makeText(signUp.this,
-                                    appUser.getUsername() + " is signed up",
-                                    Toast.LENGTH_LONG,FancyToast.SUCCESS, true).show();
-                        } else {
-                            FancyToast.makeText(signUp.this,
-                                    "There was an error, Debil: " + e.getMessage(),
-                                    Toast.LENGTH_LONG,FancyToast.ERROR, true).show();
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(signUp.this,
+                                        appUser.getUsername() + " is signed up",
+                                        Toast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            } else {
+                                FancyToast.makeText(signUp.this,
+                                        "There was an error, Debil: " + e.getMessage(),
+                                        Toast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
+                            }
+                            progressDialog.dismiss();
                         }
-                        progressDialog.dismiss();
-                    }
-                });
+                    });
+                }
                 break;
             case R.id.btnLogin:
                 Intent intent = new Intent(signUp.this, LoginActivity.class );
                 startActivity(intent);
                 break;
         }
+    }
+
+    public void rootLayoutTapped (View view ) {
+        try {
+            InputMethodManager inputMethodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e ) {
+            e.printStackTrace();
+        }
+
+
     }
 }
