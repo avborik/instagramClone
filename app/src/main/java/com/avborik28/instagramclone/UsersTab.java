5fc67e9c1f81cd21cee340295428ef11c3d625a4
AvBorikDeveloper <avboris28@gmail.com>
2020-03-27 03:36:11 +1300

    Added Pretty dialogs and long click for user info Finished practise project, well done

--- app/src/main/java/com/avborik28/instagramclone/UsersTab.java
+++ app/src/main/java/com/avborik28/instagramclone/UsersTab.java
@@ -13,12 +13,16 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 
 /**
@@ -48,6 +52,7 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
         arrayAdapter = new ArrayAdapter (getContext(), android.R.layout.simple_list_item_1, arrayList);
         txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
         listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
 
 
         ParseQuery <ParseUser> parseQuery = ParseUser.getQuery();
@@ -83,7 +88,38 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
         parseQuery.whereEqualTo("username",arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(user != null && e == null){
+                    //FancyToast.makeText(getContext(), user.get("profileProfession") + "", Toast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                    final PrettyDialog prettyDialog =  new PrettyDialog(getContext());
+                    prettyDialog.setTitle(user.getUsername() + " 's Info")
+                            .setMessage(user.get("profileBio") + "\n"
+                                    + user.get("profileProfession") + "\n"
+                                    + user.get("profileHobbies") + "\n"
+                                    + user.get("profileSport"))
+                            .setIcon(R.drawable.person)
+                            .addButton(
+                                    "OK",     // button text
+                                    R.color.pdlg_color_white,  // button text color
+                                    R.color.pdlg_color_green,  // button background color
+                                    new PrettyDialogCallback() {  // button OnClick listener
+                                        @Override
+                                        public void onClick() {
+                                            // Do what you gotta do
+                                            prettyDialog.dismiss();
+                                        }
+                                    }
+                            )
+                            .show();
 
-        return false;
+
+
+                }
+            }
+        });
+
+        return true;
     }
 }
