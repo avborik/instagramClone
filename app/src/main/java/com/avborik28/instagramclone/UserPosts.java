e999466d7b9f42104f87f603736fcd5a28c9e732
AvBorikDeveloper <avboris28@gmail.com>
2020-03-26 22:40:27 +1300

    Added UserPosts activity

---
+++ app/src/main/java/com/avborik28/instagramclone/UserPosts.java
@@ -0,0 +1,14 @@
+package com.avborik28.instagramclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserPosts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_posts);
+    }
+}
